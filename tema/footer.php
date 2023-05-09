13851513b31967c67c34af98436cb5a381606edb
Andrey Tsibin <i@atsibin.ru>
2023-05-09 18:47:02 +0500

    add fix

---
+++ tema/footer.php
@@ -0,0 +1,11 @@
+    <!-- Footer -->
+    <footer class="footer">
+      <p class="copyright"> © 2023 Все права защищены | Pokodem</p>
+    </footer>
+  </div>
+
+  <script src="./scripts/script.js"></script>
+  <script src="./scripts/prism.js"></script>
+</body>
+
+</html>
---
+++ tema/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+// Добавление стилей
+add_action( 'wp_enqueue_scripts', function () {
+  wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/styles/normalize.css' );
+  wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/styles/style.css' );
+	wp_enqueue_style( 'prism', get_template_directory_uri() . '/assets/styles/prism.css' );
+
+
+
+	// wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+});
+
+?>
