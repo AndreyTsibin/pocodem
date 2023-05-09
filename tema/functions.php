954e6cc2c626b5fce439353f32cc4254d7b5d2a4
Andrey Tsibin <i@atsibin.ru>
2023-05-09 18:58:10 +0500

    add fix

--- tema/functions.php
+++ tema/functions.php
@@ -5,6 +5,8 @@ add_action( 'wp_enqueue_scripts', function () {
   wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/styles/normalize.css' );
   wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/styles/style.css' );
 	wp_enqueue_style( 'prism', get_template_directory_uri() . '/assets/styles/prism.css' );
+  wp_enqueue_style( 'post', get_template_directory_uri() . '/assets/styles/post.css' );
+  wp_enqueue_style( 'political', get_template_directory_uri() . '/assets/styles/style-gen-politica.css' );
 
 
 
