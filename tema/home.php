608eee3a535ba3c85173a45b5c1c800e5e0f477b
Andrey Tsibin <i@atsibin.ru>
2023-05-10 08:23:19 +0500

    add

--- tema/home.php
+++ tema/home.php
@@ -5,11 +5,12 @@ Template Name: home
 ?>
 <?php get_header(); ?>
 
+
     <!-- Content -->
     <main class="content">
       <!-- Обложка -->
       <section class="main__slider">
-        <img class="main__logo" src="./images/logo.svg" alt="Логотип">
+        <img class="main__logo" src="<?php bloginfo('template_url'); ?>/assets/images/logo.svg" alt="Логотип">
         <h1 class="heading">Инструменты для разработки веб-сайтов</h1>
       </section>
       <!-- Тильда -->
@@ -24,21 +25,21 @@ Template Name: home
 
           <div class="searchable">
             <a class="main__content-card" href="post/kak-zakrepit-shapku-v-zero.html">
-              <img class="main__content-img" src="./images/timg1.svg" alt="Тильда">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/timg1.svg" alt="Тильда">
               <h3 class="main__card-title">Как сделать фиксированное меню в Zero Block?</h3>
             </a>
           </div>
 
           <div class="searchable">
             <a class="main__content-card" href="#">
-              <img class="main__content-img" src="./images/timg2.svg" alt="Тильда">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/timg2.svg" alt="Тильда">
               <h3 class="main__card-title">Как скруглить углы у стандартного pop-up на Тильде</h3>
             </a>
           </div>
 
           <div class="searchable">
             <a class="main__content-card" href="#">
-              <img class="main__content-img" src="./images/timg3.svg" alt="Тильда">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/timg3.svg" alt="Тильда">
               <h3 class="main__card-title">Как заменить цвета шапки в Zero блок по скроллу на Тильде</h3>
             </a>
           </div>
@@ -57,21 +58,21 @@ Template Name: home
 
           <div class="searchable">
             <a class="main__content-card" href="#">
-              <img class="main__content-img" src="./images/js-1.svg" alt="JavaScript">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/js-1.svg" alt="JavaScript">
               <h3 class="main__card-title">Генерация случайных чисел в JavaScript</h3>
             </a>
           </div>
 
           <div class="searchable">
             <a class="main__content-card" href="#">
-              <img class="main__content-img" src="./images/js-2.svg" alt="JavaScript">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/js-2.svg" alt="JavaScript">
               <h3 class="main__card-title">Как объединить массивы в JavaScript</h3>
             </a>
           </div>
 
           <div class="searchable">
             <a class="main__content-card" href="#">
-              <img class="main__content-img" src="./images/js-3.svg" alt="JavaScript">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/js-3.svg" alt="JavaScript">
               <h3 class="main__card-title">Как сложить все числа в массиве JavaScript?</h3>
             </a>
           </div>
@@ -90,21 +91,21 @@ Template Name: home
 
           <div class="searchable">
             <a class="main__content-card" href="post/generator-politiki.html">
-              <img class="main__content-img" src="./images/g-1.svg" alt="JavaScript">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/g-1.svg" alt="JavaScript">
               <h3 class="main__card-title">Генератор политики конфиденциальности</h3>
             </a>
           </div>
 
           <div class="searchable">
             <a class="main__content-card" href="#">
-              <img class="main__content-img" src="./images/g-2.svg" alt="JavaScript">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/g-2.svg" alt="JavaScript">
               <h3 class="main__card-title">CSS генератор тени для сайта box shadow</h3>
             </a>
           </div>
 
           <div class="searchable">
             <a class="main__content-card" href="#">
-              <img class="main__content-img" src="./images/g-3.svg" alt="JavaScript">
+              <img class="main__content-img" src="<?php bloginfo('template_url'); ?>/assets/images/g-3.svg" alt="JavaScript">
               <h3 class="main__card-title">CSS-генератор эффекта матового стекла Glassmorphism</h3>
             </a>
           </div>
